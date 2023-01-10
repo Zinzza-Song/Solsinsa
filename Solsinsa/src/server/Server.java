f240b73c21648a72846c3b3efabea313221e06c5
Han-Bin Song <thdgksq@naver.com>
2023-01-10 18:11:20 +0900

    서버개발ing1
    서버 개발 진행 중

--- Solsinsa/src/server/Server.java
+++ Solsinsa/src/server/Server.java
@@ -1,9 +1,28 @@
 package server;
 
-public class Server {
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server implements Runnable {
+
+	private static Socket client;
+	static PrintWriter out = null;
+	static ArrayList<Socket> list = new ArrayList<Socket>();
+	static int UserNum = 0;
+	
+	public Server(Socket client) {
+		this.client = client;
+		list.add(client);
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 

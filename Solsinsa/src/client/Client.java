7d732eb877446e0d55c6cdb5e0d813275b7c8c41
Han-Bin Song <thdgksq@naver.com>
2023-01-14 23:24:07 +0900

    상품 데이터 직렬화 완료
    상품데이터(상세 상품 데이터 까지) 직렬화 하여 클라이언트가 실행하면 서버에서 받아와서 해당 데이터를 담은 객체들을 리스트로 관리하게끔 설정 그리고 처음 화면에서 랜덤으로 상품4개 띄우는데 성공

--- Solsinsa/src/client/Client.java
+++ Solsinsa/src/client/Client.java
@@ -2,47 +2,67 @@ package client;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.security.spec.MGF1ParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.concurrent.ForkJoinTask;
 
-public class Client extends Thread{
+import server.Bottom;
+import server.Outer;
+import server.Product;
+import server.Top;
+
+public class Client extends Thread {
 
 	Socket socket;
 	BufferedReader br;
 	PrintWriter out;
 	Scanner sc;
-	
+
 	public static String msg = null;
 	public static String ans = null;
 	
+	public static ArrayList<Product> products;
+	public static ArrayList<Top> tops;
+	public static ArrayList<Outer> outers;
+	public static ArrayList<Bottom> bottoms;
+
 	final String localhost = "127.0.0.1";
-	
+
 	@Override
 	public void run() {
 		System.out.println("Connecting...");
 
-		try{
+		try {
 			// 서버의 accept()가 호출 후 쓰레드가 만들어지고, List에 추가됨
 			socket = new Socket(localhost, 7048); // 7048포트로 서버와 연결
+
+			out = new PrintWriter(socket.getOutputStream(), true);
+
+			System.out.println("Connected!!");
+			out.println("start:1000");
+			
+			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+			HashMap<Object, Object> map = (HashMap<Object, Object>) ois.readObject();
+			products = (ArrayList<Product>) map.get("product");
+			tops = (ArrayList<Top>)map.get("top");
+			outers = (ArrayList<Outer>)map.get("outer");
+			bottoms = (ArrayList<Bottom>)map.get("bottom");
+
 			SocketThread thread = new SocketThread(); // 메시지 수신을 위한 쓰레드 객체 생성
 			thread.start(); // 생성한 쓰레드 객체 실행
-			
-			out = new PrintWriter(socket.getOutputStream(), true);
-			
-			System.out.println("Connected!!");
- 
+
 			while (true) {
 				sleep(1000);
-				if(msg != null) {
+				if (msg != null) {
 					System.out.println("요청할 메시지 : " + msg);
 					out.println(msg); // 입력받은 키보드 값을 서버로 전송
-					
-					if(msg.equals("end"))
+
+					if (msg.equals("end"))
 						break;
-					
+
 					msg = null;
 				}
 			}
@@ -59,10 +79,10 @@ public class Client extends Thread{
 		@Override
 		public void run() {
 			try {
-				while(!this.isInterrupted()) {
+				while (!this.isInterrupted()) {
 					br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 					String res = null; // 수신된 메시지를 저장
-					while((res = br.readLine()) != null) { // br에 읽을 메시지가 있는 한 무한루프
+					while ((res = br.readLine()) != null) { // br에 읽을 메시지가 있는 한 무한루프
 						ans = res;
 						System.out.println("응답" + ans);
 					}
@@ -72,7 +92,7 @@ public class Client extends Thread{
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 }
